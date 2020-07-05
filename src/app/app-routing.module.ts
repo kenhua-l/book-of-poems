931db60ba805d8b882e9cf134995d2337f3ba210
kenhua-l <l_kenhua93@hotmail.com>
2020-07-05 18:04:15 +0800

    routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PoemsComponent } from './poems/poems.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'poems', component: PoemsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
